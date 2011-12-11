59178622ebd79c037a78fc5eb28495d9808f2242
Morten Wilken <wilken@ultraplex.org>
2011-12-11 13:59:41 +0100

    A little progress

--- js/jquery.ui.placepicker.js
+++ js/jquery.ui.placepicker.js
@@ -6,6 +6,8 @@
 			width: "100%",
 			height: "300px",
 			zoom: 5,
+			_interal_longtitude: null,
+			_interal_latitude: null,
 		},
 		shared: {
 		  popup:null,
@@ -19,15 +21,18 @@
 			$button=$("<img src='http://www.google.com/mapfiles/marker.png'></img>")
 			self.element.html($button)
 			$button.before($text)
-			self._createPopup(self)
 
 			$button.click(function() {
+  			self._createPopup(self)
 	   		
+	   		self.options._interal_longtitude = self.options.longtitude
+        self.options._interal_latitude = self.options.latitude
+        
 				self.shared.popup.css({left:self.element.offset().left+200})
 				self.shared.popup.css({top:self.element.offset().top})
 				self.shared.popup.appendTo(self.element)
 				
-				self.shared.latLng = new google.maps.LatLng(self.options.latitude,self.options.longtitude)
+				self.shared.latLng = new google.maps.LatLng(self.options._interal_latitude,self.options._interal_longtitude)
   			self.shared.mapOptions = {
   				zoom: self.options.zoom,
   				center: self.shared.latLng,
@@ -63,14 +68,14 @@
 			google.maps.event.addListener(self.shared.map, 'click', function(event) {
 		   		self._placeMarker(self, event.latLng);
 				  geocoder.geocode({latLng:event.latLng}, function(results, status) {
-					  self.options.latitude=event.latLng.lat()
-					  self.options.longtitude=event.latLng.lng()
+					  self.options._internal_latitude=event.latLng.lat()
+					  self.options._internal_longtitude=event.latLng.lng()
 			      if (status == google.maps.GeocoderStatus.OK) {
 				   	  if (results[0]) {
-				     	  self.shared.popup.find("input.adress").val(results[0].formatted_address)
+				     	  self.shared.popup.find("input.address").val(results[0].formatted_address)
 				   	  }
 				    } else {
-			    	  self.shared.popup.find("input.adress").val("")
+			    	  self.shared.popup.find("input.address").val("")
 				 	  }
 				})
 				
@@ -79,14 +84,33 @@
 		},
 		_placeMarker: function(self, location) {
 			self.shared.marker.setMap(self.shared.map)
-			self.shared.marker.setPosition(location)
-			self.shared.map.panTo(location);
+			
+			if(location.getNorthEast){
+  			self.shared.marker.setPosition(location.getCenter())
+  			self.shared.map.panToBounds(location);
+			}else{
+  			self.shared.marker.setPosition(location)
+  			self.shared.map.panTo(location);
+			}
 		},
 		_createPopup: function(self){
-			self.shared.popup=$("<div class='popup'><div class='map'>loading</div><div><input class='adress' type='text' value='type location'><input type='button' value='search'><input type='button' value='submit'></div></div>")
+			self.shared.popup=$("<div class='popup'><div class='map'>loading</div><div><input class='address' type='text' value='type location'><input type='button' value='search'><input type='button' value='submit'></div></div>")
 			self.shared.popup.css({width:'400px'})
+			self.shared.popup.find("input[value='submit']").click(function(){
+        self.options.longtitude = self.options._internal_longtitude
+        self.options.latitude = self.options._internal_latitude
+  			self.shared.popup.remove()
+      })
+			self.shared.popup.find("input[value='search']").click(function(){
+			  var geocoder=new google.maps.Geocoder();
+			  geocoder.geocode({address: self.shared.popup.find("input.address").val()}, function(results, status) {
+				  self._placeMarker(self, results[0].geometry.location)
+				  self.shared.popup.find("input.address").val(results[0].formatted_address)
+			  })
+      })
+      
 			self.shared.popup.find("div.map").css({width: self.options.width,height: self.options.height})
-			self.shared.popup.find("input.adress").css({width:'281px',display:'inline'})
+			self.shared.popup.find("input.address").css({width:'281px',display:'inline'})
 			self.shared.popup.addClass('ui-dialog ui-corner-all')
 			self.shared.popup.addClass('ui-widget-content')
 			self.shared.popup.click(function(){
